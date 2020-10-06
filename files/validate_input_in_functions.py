ca4c5d9c5019269e82ae269c87d88afc8b312b5d
Reser, Jack C <jcreser@dmacc.edu>
2020-10-05 23:35:47 -0500

    added tests and docstring

---
+++ files/validate_input_in_functions.py
@@ -0,0 +1,11 @@
+""" This validates input and formats the output of test_name and test_score, or invalid output
+    :param test_name, a string with a test name
+    :param test_score, optional int between 0-100. default = 0
+    :param invalid_message, optional string with message to output if input is invalid. default = 'Invalid test score, try again!'
+    :returns the formatted output with test_name and validated test_score. Else returns invalid_message
+    """
+
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    # return {test_name:test_score}
+    pass
---
+++ test/test_valid_input_in_functions.py
@@ -0,0 +1,31 @@
+import unittest
+from files.validate_input_in_functions import score_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual(score_input("Python test #1"), "Python test #1: 0")
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual(score_input("Python test #1", 50), "Python test #1: 50")
+        self.assertEqual(score_input("Python test #2", 83), "Python test #2: 83")
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual(score_input("Python test #3", -43), "Invalid test score, try again!")
+        self.assertEqual(score_input("Python test #4", -1), "Invalid test score, try again!")
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual(score_input("Python test #7", 101), "Invalid test score, try again!")
+        self.assertEqual(score_input("Python test #7", 10534), "Invalid test score, try again!")
+
+    def test_test_score_non_numeric(self):
+        self.assertEqual(score_input("Python test #1", "me"), "Invalid test score, try again!")
+        self.assertEqual(score_input("Python test #1", ";'."), "Invalid test score, try again!")
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual(score_input("Python test #1"), 98, "INVALID TEST SCORE!!!!", "Python test #1: 98")
+        self.assertEqual(score_input("Python test #5"), 143, "INVALID TEST SCORE!!!!", "INVALID TEST SCORE!!!!")
+
+
+if __name__ == '__main__':
+    unittest.main()
