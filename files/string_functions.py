19ed5ea1c59c96a01d6e0419c21abfd0986cfef6
Reser, Jack C <jcreser@dmacc.edu>
2020-10-05 22:50:30 -0500

    added docstring

--- files/string_functions.py
+++ files/string_functions.py
@@ -1,3 +1,9 @@
+""" This multiplies the string name by num
+    :param name, a string with a name
+    :param num, an int between 1-9
+    :returns the new string with name, num of times
+    """
+
 
 def multiply_string(name, num):
     return name*num
